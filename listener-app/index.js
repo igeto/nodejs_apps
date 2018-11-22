01047badc6edeec1cff35bf653c22220fe474d35
igeto <igetom@gmail.com>
2018-11-22 15:22:36 +0100

    added first project listener-app

---
+++ listener-app/index.js
@@ -0,0 +1,39 @@
+/**
+ * Primary file for the API
+ * 
+ */
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    // Get the http Method
+    const method = req.method.toLowerCase();
+
+    // Get the headers as an object
+    const headers = req.headers;
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    // console.log(`Request is received on path: ${trimmedPath} with this method: ${method} and with these query string params: `, queryStringObject)
+    console.log(`Request received with these headers: `, headers)
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, () => {
+    console.log('The server is listening on port 3000 now');
+});
